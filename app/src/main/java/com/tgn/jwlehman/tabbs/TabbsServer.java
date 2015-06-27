2e8cea1fd55ac842b48b9805f0eb10ab7b0b0159
Jeremy Lehman <jwlehman@purdue.edu>
2015-06-27 19:09:19 -0400

    add constructors for server socket implementation

--- app/src/main/java/com/tgn/jwlehman/tabbs/TabbsServer.java
+++ app/src/main/java/com/tgn/jwlehman/tabbs/TabbsServer.java
@@ -1,13 +1,38 @@
 package com.tgn.jwlehman.tabbs;
 
 /**
- * Created by jwlehman on 6/27/15.
+ * Created by jwlehman(Jeremy Lehman), jwlehman@purdue.edu on 6/27/15.
+ * Server for Tabbs app to handle usernames/passwords and any data
  */
 
 import java.io.*;
 import java.net.*;
-public class TabbsServer {
+public class TabbsServer extends Thread {
 
-    
+    private ServerSocket serverSocket;
+    /*
+    author: jwlehman, jwlehman@purdue.edu
+    param: port where socket will be created
+    Desc: constructor that takes a port to create a new server
+     */
+    public TabbsServer(int port) throws SocketException, IOException{
+        serverSocket = new ServerSocket(port);
+        System.out.println("Server is bound to port " + port);
+        serverSocket.setReuseAddress(true);
+    }
+
+    /*
+    author: jwlehman, jwlehman@purdue.edu
+    param: none
+    Desc: constructor that assigns port for new server
+     */
+    public TabbsServer() throws SocketException, IOException {
+        serverSocket = new ServerSocket(0);
+        System.out.println("Server is boud to port " + serverSocket.getLocalPort());
+        serverSocket.setReuseAddress(true);
+    }
+
+
+
 
 }
